f7688449690cec83ffe126f9f4fca53d05a7b403
Patryk Stępnik <stepnikpa@gmail.com>
2023-03-26 14:06:01 +0200

    Implemented "Concrete Class Demo"

---
+++ ConcreteClassDemoLibrary/Models/RegularEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcreteClassDemoLibrary.Models
+{
+    internal class RegularEmployee : Employee
+    {
+        public RegularEmployee(string name, string position) 
+        {
+            Name = name;
+            Position = position;
+        }
+
+        // Class overrides Work method, but actually it's calling Employee.Work method using "base" keyword
+        internal override void Work() => base.Work();
+
+        // Class does not override FinishWork method.
+        // We still can call this method and use implementation provided by Employee class.
+
+    }
+}
---
+++ ConcreteClassDemoLibrary/Workflows.cs
@@ -0,0 +1,80 @@
+﻿using ConcreteClassDemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConcreteClassDemoLibrary
+{
+    public static class Workflows
+    {
+        public static void ClassOverviewWorkflow()
+        {
+            // Instantiating an object
+            Employee employee = new Employee("Patryk");
+
+            // Updating object property
+            employee.Position = "Dev";
+
+            // Calling object methods
+            employee.StartWork();
+            employee.PerformWork(4);
+            employee.ReportHours();
+            employee.FinishWork();
+        }
+
+        public static void ClassInheritanceWorkflow()
+        {
+            // Creating List of Employee objects, this is possibla, because
+            // Manager and Regular Employee inherit from Employee class
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee("Patryk", "Dev"),
+                new Manager("Jan"),
+                new RegularEmployee("Stefan", "Regular Employee")
+            };
+
+            
+            foreach (Employee employee in employees)
+            {
+                employee.StartWork();
+
+                // Reflection - reading object runtime type and casting Employee type to Manager type
+                if (employee.GetType() == typeof(Manager))
+                {
+                    ((Manager)employee).AllocateTasks();
+                }
+
+                // Method outputs match implementations provided by proper class
+                // regardless of iterating through them as Employee objects
+                employee.PerformWork(4);
+                employee.ReportHours();
+                employee.FinishWork();
+                Console.WriteLine();
+            }
+        }
+
+        public static void OperationsOrderWorkflow()
+        {
+            Console.WriteLine("Calling \"Manager(string name) : base(name, \"Manager\")\" constructor.\n");
+            
+            Manager manager = new Manager("Patryk");
+
+            Console.WriteLine("Operations order:");
+            Console.WriteLine("1. Employee(string name, string position) constructor set both Name and Position properties.");
+            Console.WriteLine("2. Manager constructor does nothing.");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Calling \"RegularEmployee(string name, string position)\" constructor.\n");
+            
+            RegularEmployee employee = new RegularEmployee("Patryk", "Dev");
+            Console.WriteLine("Operations order:");
+            Console.WriteLine("1. Employee() constructor set Position property value to \"Unassigned\".");
+            Console.WriteLine("2. RegularEmployee set both Name and Position properties.\n");
+            Console.WriteLine("WARNING: in this case RegularEmployee constructor overrides value provided by Employee constructor.");
+        }
+    }
+}
