91dd80477d48b770e04f6c17c0c0f17b3ba3d6c9
Patryk Stępnik <stepnikpa@gmail.com>
2023-03-26 15:51:26 +0200

    Implemented Abstract Class demo

---
+++ AbstractClassDemoLibrary/Menu.cs
@@ -0,0 +1,18 @@
+﻿using AbstractClassDemoLibrary.Services;
+
+
+namespace AbstractClassDemoLibrary
+{
+    public static class Menu
+    {
+        public static Dictionary<int, string> MenuList { get; private set; } = new Dictionary<int, string>()
+        {
+            { 1, "1 - Overview" },
+        };
+
+        public static Dictionary<int, Action> MenuActions { get; private set; } = new Dictionary<int, Action>()
+        {
+            { 1, Workflows.AbstractClassOverview },
+        };
+    }
+}
---
+++ AbstractClassDemoLibrary/Models/RegularEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassDemoLibrary.Models
+{
+    internal class RegularEmployee : Employee
+    {
+        public RegularEmployee(string name) : base(name) { }
+
+        // overriding base class method, but actually calling the base class implementation
+        internal override void StartWork()
+        {
+            base.StartWork();
+        }
+
+        // Implementing abstract class RegisterHours method
+        internal override void RegisterHours() => Console.WriteLine($"Sending tasks summary report to superior.");
+        
+    }
+}
