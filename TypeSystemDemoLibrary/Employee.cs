29c0c401109b2736be9794109666e1fde1da5718
Patryk Stępnik <stepnikpa@gmail.com>
2023-03-26 11:58:11 +0200

    Implemented: examples for System Type demo library, menu for UI

---
+++ TypeSystemDemoLibrary/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace TypeSystemDemoLibrary
+{
+    internal class Employee
+    {
+        internal string Name { get; set; }
+        internal string Position { get; set; }
+
+        internal Employee(string name, string position)
+        {
+            Name = name;
+            Position = position;
+        }
+
+        public override string ToString() => $"{Name} - {Position}";
+    }
+}
